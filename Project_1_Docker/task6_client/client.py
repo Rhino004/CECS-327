cef7e681dceb0086f76257af2eb81b24aa1e9a8b
Ryan Tomas <ryantomas85@gmail.com>
2025-09-18 16:58:07 -0700

    changing the print statments for server and clients

--- Project_1_Docker/task6_client/client.py
+++ Project_1_Docker/task6_client/client.py
@@ -6,9 +6,9 @@ TCP_port = 5000
 def tcp_client():
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server:
         server.connect((host, TCP_port)) #connect with the server
-        server.sendall(b"Hello from TCP client") #send a message to TCP server
+        server.sendall(b"Hello from TCP client that is using Docker") #send a message to TCP server
         data = server.recv(1024)
-        print(f"TCP Client Received: {data.decode()}") #The client would decode the data from the server
+        print(f"TCP Client Received from the TCP Server: {data.decode()}") #The client would decode the data from the server
 
 time.sleep(3) 
 tcp_client()
--- Project_1_Docker/task6_server/server.py
+++ Project_1_Docker/task6_server/server.py
@@ -7,20 +7,20 @@ TCP_port = 5000
 UDP_port = 5001
 #connects the TCP clients in its own thread
 def handle_tcp(conn,addr):
-    print(f"TCP Connected by {addr}")
+    print(f"TCP Connected by the address {addr}")
     with conn: #checks if there is a connection
         while True:
             data = conn.recv(1024)#1024 is the bytes
             if not data: #close the connection if there is not 
                 break
-            print(f"TCP Received from {addr}: {data.decode()}")
+            print(f"TCP Received from  address {addr}: {data.decode()}")
             conn.sendall(data)#echo message to the client
 
 def tcp_server():
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server:
         server.bind((host, TCP_port)) #
         server.listen()
-        print(f"TCP Listening on {host}:{TCP_port}")
+        print(f"TCP Listening on the host and port: {host}:{TCP_port}")
         while True:
             conn, addr = server.accept()#accetps new client
             #threading is used to give access to multiple clients at the same time
