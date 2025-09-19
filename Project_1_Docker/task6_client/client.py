9b0394496ed7621ce9b14ffeb5f95dc1801beed9
Ryan Tomas <113748661+Rhino004@users.noreply.github.com>
2025-09-18 20:27:50 -0700

    changing the way client.py is doing the logic

--- Project_1_Docker/task6_client/client.py
+++ Project_1_Docker/task6_client/client.py
@@ -3,13 +3,11 @@ import socket, time
 host = "server"  
 TCP_port = 5000
 
-def tcp_client():
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server:
+time.sleep(3) 
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server:
         server.connect((host, TCP_port)) #connect with the server
         server.sendall(b"Hello from TCP client that is using Docker") #send a message to TCP server
         data = server.recv(1024)
         print(f"TCP Client Received from the TCP Server: {data.decode()}") #The client would decode the data from the server
 
-time.sleep(3) 
-tcp_client()
 
