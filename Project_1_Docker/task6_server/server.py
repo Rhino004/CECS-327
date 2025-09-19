23820c4fb7767b7bb30156f7bb60740c8a174208
Ryan Tomas <113748661+Rhino004@users.noreply.github.com>
2025-09-18 19:48:38 -0700

    changing the CMD in the server dockerfile to make the prints appear in the docker compose

--- Project_1_Docker/task6_server/server.py
+++ Project_1_Docker/task6_server/server.py
@@ -6,15 +6,16 @@ host = '0.0.0.0'
 TCP_port = 5000
 UDP_port = 5001
 #connects the TCP clients in its own thread
-def handle_tcp(conn,addr):
+def handle_tcp(connection,addr):
     print(f"TCP Connected by the address {addr}")
-    with conn: #checks if there is a connection
+    with connection: #checks if there is a connection
+        connection.sendall(b"This is the TCP server in the Docker container")
         while True:
-            data = conn.recv(1024)#1024 is the bytes
+            data = connection.recv(1024)#1024 is the bytes
             if not data: #close the connection if there is not 
                 break
             print(f"TCP Received from  address {addr}: {data.decode()}")
-            conn.sendall(data)#echo message to the client
+            connection.sendall(data)#echo message to the client
 
 def tcp_server():
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server:
@@ -22,9 +23,9 @@ def tcp_server():
         server.listen()
         print(f"TCP Listening on the host and port: {host}:{TCP_port}")
         while True:
-            conn, addr = server.accept()#accetps new client
+            connection, addr = server.accept()#accetps new client
             #threading is used to give access to multiple clients at the same time
-            threading.Thread(target=handle_tcp, args=(conn, addr)).start()
+            threading.Thread(target=handle_tcp, args=(connection, addr)).start()
 
 
 
